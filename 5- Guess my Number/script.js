7e3dee4be1ff03a39727c5e49e0af36830386276
Jesus <ibraheemhamodeh@gmail.com>
2022-03-07 21:20:49 +0300

    Guess the Number game done

--- 5- Guess my Number/script.js
+++ 5- Guess my Number/script.js
@@ -1,4 +1,39 @@
 'use strict';
+let score = 20;
+let highScore = 0;
+const randomNum = Math.floor(Math.random() * 19) + 1;
+console.log(randomNum);
+console.log(`You fucking cheater close the console and play fairly`);
+// let message = document.querySelector('.message').textContent;
+// let scoreText = document.querySelector('.label-score').textContent;
+// let highScoreText = document.querySelector('.label-highscore').textContent;
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
 
-console.log('hi');
-console.log('hi');
+  if (!guess) {
+    document.querySelector('.message').textContent =
+      'Please enter a valid number';
+  } else if (guess != randomNum && score === 1) {
+    document.querySelector('.message').textContent = 'Congrats,You Lost 🦍';
+    score--;
+    document.querySelector('.label-score').textContent = `💯 Score: ${score}`;
+  } else if (guess > randomNum) {
+    document.querySelector('.message').textContent = '📈 Too high';
+    score--;
+    document.querySelector('.label-score').textContent = `💯 Score: ${score}`;
+  } else if (guess < randomNum) {
+    document.querySelector('.message').textContent = '📉 Too low';
+    score--;
+    document.querySelector('.label-score').textContent = `💯 Score: ${score}`;
+  } else {
+    document.querySelector('.message').textContent = 'YOU GOT IT WOHOOOOO!';
+    document.querySelector('.number').textContent = randomNum;
+
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector(
+        '.label-highscore'
+      ).textContent = `🥇 Highscore: ${score}`;
+    }
+  }
+});
